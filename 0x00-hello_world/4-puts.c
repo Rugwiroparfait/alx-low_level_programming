5279ba13ec863688d1e3faf6187e455e3699d4ec
BLACKLEGEN <rugwiroparfait003@gmail.com>
2023-08-11 05:14:46 -0700

    4-puts.c added'

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase "Programming is like build"
- * Return 0 if exited properly non-zero otherwise
+ * main - Entry point of the program
+ *
+ * Description: This function prints the phrase "Programming is like
+ * building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
+
