059bfe4c41c0cc5d0f26d2a5c18914bbf0d6b128
BLACKLEGEN <rugwiroparfait003@gmail.com>
2023-08-15 01:33:22 -0700

    debug

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,17 +1,8 @@
-#include <stdio.h>
-
-int main() {
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            for (int k = 0; k < 10; k++) {
-                for (int l = 0; l < 10; l++) {
-                    for (int m = 0; m < 10; m++) {
-                        putchar('\n');
-                    }
-                }
-            }
-        }
-    }
-    putchar('\n');
-    return 0;
+}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
