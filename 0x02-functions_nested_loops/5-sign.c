389c9e65fedf0496ea6ad0e23ed30dca13ac8a8a
BLACKLEGEN <rugwiroparfait003@gmail.com>
2023-08-16 09:00:54 -0700

    new collection

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 
 /**
@@ -10,18 +9,19 @@
  */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	} else if (n == 0)
+    if (n > 0)
+    {
+        _putchar('+');
+        return (1);
+    }
+    else if (n == 0)
+    {
+        _putchar('0');
+        return (0);
+    }
+    else if (n < 0)
+    {
+        _putchar('-');
+        return (-1);
+    }
 }
-		_putchar(48);
-		return (0);
-	} else if (n < 0)
-	{
-		_putchar('-');
-	}
-		return (-1);
-}
-
