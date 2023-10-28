731ce4083c0567f18a0f94c169c8cdf304ce8f9b
Rugwiroparfait <rugwiroparfait003@gmail.com>
2023-10-28 12:32:40 +0200

    debugged!

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -7,6 +7,5 @@
  */
 int _isupper(int c)
 {
-    return (c >= 'A' && c <= 'Z');
+	return (c >= 'A' && c <= 'Z');
 }
-
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+/*a list that was given from the task 0 */
 
 int _putchar(char c);
 int _islower(int c);
@@ -20,5 +22,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle)
+char *_strstr(char *haystack, char *needle);
+
 #endif
