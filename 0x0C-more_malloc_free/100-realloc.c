13cecdf2cc479b20a6a8d1c44e4f5a7c30993cde
Rugwiroparfait <rugwiroparfait003@hmail.com>
2023-11-03 06:52:59 -0700

    debugging

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,28 +17,28 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr == NULL)
 	{
-		temp_block = malloc(new_size);
-		return (temp_block);
+	temp_block = malloc(new_size);
+	return (temp_block);
 	}
 	else if (new_size == old_size)
-		return (ptr);
+	return (ptr);
 
 	else if (new_size == 0 && ptr != NULL)
 	{
-		free(ptr);
-		return (NULL);
+	free(ptr);
+	return (NULL);
 	}
 	else
 	{
-		temp_block = malloc(new_size);
-		if (temp_block != NULL)
-		{
-			for (i = 0; i < min(old_size, new_size); i++)
-				*((char *)temp_block + i) = *((char *) ptr + i);
-			free(ptr);
-			return (temp_block);
-		}
-		else
-			return (NULL);
+	temp_block = malloc(new_size);
+	if (temp_block != NULL)
+	{
+	for (i = 0; i < min(old_size, new_size); i++)
+	*((char *)temp_block + i) = *((char *) ptr + i);
+	free(ptr);
+	return (temp_block);
+	}
+	else
+	return (NULL);
 	}
 }
