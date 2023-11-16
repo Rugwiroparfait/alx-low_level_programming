3629b07d2d4bbef25a08f0da1154a4ea1b4a4e0f
Rugwiroparfait <rugwiroparfait003@gmail.com>
2023-11-17 00:05:57 +0200

    101-hello_holberton.

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,29 +1,26 @@
-section .data
-    hello db 'Hello, Holberton', 0
+; File: 101-hello_holberton.asm
+; Auth: Rugwiro parfait
+; Desc: 64-bit assembly program that prints
+;       Hello, Holberton followed by a new line.
 
-section .text
-    global _start
-
-_start:
-    ; Call printf
-    mov rdi, hello
-    mov rax, 0          ; 0 is the syscall number for sys_write
-    call printf
-
-    ; Exit the program
-    mov rax, 60         ; 60 is the syscall number for sys_exit
-    xor rdi, rdi        ; Exit code 0
-    syscall
-
-section .text
-global printf
 extern printf
 
-printf:
-    mov eax, 0          ; syscall: sys_write
-    mov edi, 1          ; file descriptor: stdout
-    mov edx, 17         ; length of the message
-    mov esi, hello      ; pointer to the message
-    syscall
-    ret
+section .text
+   global main
 
+main:
+   push rbp
+
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0
+   call printf
+
+   pop rbp
+
+   mov rax,0
+   ret
+
+section .data
+   msg: db "Hello, Holberton", 0
+   fmt: db "%s", 10, 0
