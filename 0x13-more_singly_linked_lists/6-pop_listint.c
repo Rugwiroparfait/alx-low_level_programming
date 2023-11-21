37c02008b6e615dd52ef0a8e47d7e518123baa9e
Rugwiroparfait <rugwiroparfait003@gmail.com>
2023-11-22 00:58:23 +0200

    others

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,25 +1,26 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * pop_listint - Deletes the head node of a listint_t linked list.
- * @head: Pointer to the pointer to the head of the list.
- * 
- * Return: The head node’s data (n), or 0 if the list is empty.
+ * pop_listint - Deletes the head node of a listint_t list.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ *
+ * Return: If the linked list is empty - 0.
+ *         Otherwise - The head node's data (n).
  */
 int pop_listint(listint_t **head)
 {
-    listint_t *temp;
-    int data;
+	listint_t *tmp;
+	int ret;
 
-    if (*head == NULL)
-        return (0);
+	if (*head == NULL)
+		return (0);
 
-    temp = *head;
-    *head = (*head)->next;
-    data = temp->n;
-    free(temp);
+	tmp = *head;
+	ret = (*head)->n;
+	*head = (*head)->next;
 
-    return (data);
+	free(tmp);
+
+	return (ret);
 }
-
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,20 +1,25 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * get_nodeint_at_index - Returns the nth node of a listint_t linked list.
- * @head: Pointer to the head of the list.
- * @index: Index of the node, starting at 0.
- * 
- * Return: The nth node of the list, or NULL if the node does not exist.
+ * get_nodeint_at_index - Locates a given node of
+ *                        a listint_t linked list.
+ * @head: A pointer to the head of the listint_t list.
+ * @index: The index of the node to locate - indices start at 0.
+ *
+ * Return: If the node does not exist - NULL.
+ *         Otherwise - the located node.
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i;
+	unsigned int node;
 
-	for (i = 0; i < index && head != NULL; i++)
-	head = head->next;
+	for (node = 0; node < index; node++)
+	{
+		if (head == NULL)
+			return (NULL);
+
+		head = head->next;
+	}
 
 	return (head);
 }
-
