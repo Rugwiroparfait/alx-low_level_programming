727ba33da709f2b8b957c8b7e168424701101a12
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-01-19 01:26:44 +0200

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -P  
+wget -P .. https://raw.githubusercontent.com/Rugwiroparfait/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
+export LD_PRELOAD="$PWD/.../libhack.so"
